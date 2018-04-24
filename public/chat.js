60def236f2b1283534d644df84338c9db8232642
amir <amir_ansari50@yahoo.in>
2018-04-25 02:19:52 +0530

    first commit

---
+++ public/chat.js
@@ -0,0 +1,22 @@
+//Make Connection
+
+var socket = io.connect()
+
+//Query Dom 
+var message = document.getElementById('message');
+var handle = document.getElementById('handle');
+var btn = document.getElementById('send');
+var output = document.getElementById('output');
+
+btn.addEventListener('click',function(){
+	socket.emit('chat',{
+		handle: handle.value,
+		message: message.value
+	});
+});
+
+
+//Listen 
+socket.on('chat',function(data){
+	output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>'; 
+})
